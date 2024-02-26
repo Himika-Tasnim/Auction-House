402c6e21b247152573154e607a6154e98ea583e0
Himika-Tasnim <himikatasnim2002@gmail.com>
2024-02-27 01:57:59 +0600

    new update by himika

--- AuctionHouse/AuctionHouse/settings.py
+++ AuctionHouse/AuctionHouse/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'AuctionHouse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
